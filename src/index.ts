21068a15c4d6c00c0d3bbcd29863fbbdbefbbcfd
urmee04 <urmee04sust@gmail.com>
2025-07-13 15:11:14 -0400

    centralize imports from apiSimulator for product, reviews, and sales modules

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+// Write a Function to Handle API Calls and Display Data:
+
+/*Use fetchProductCatalog() to fetch product details and display them.
+For each product, fetch the reviews using fetchProductReviews(productId).
+After fetching products and reviews, retrieve the sales report using fetchSalesReport().*/
+import {
+  Product,
+  Reviews,
+  Sales,
+  fetchProductCatalog,
+  fetchProductReviews,
+  fetchSalesReport,
+} from "./apiSimulator";
