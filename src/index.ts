b6a57d7d1f4f2928aa4d15aa1b439c4ade60c737
urmee04 <urmee04sust@gmail.com>
2025-07-14 18:06:13 -0400

    implement error handling for API calls using Promises

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@
 /*Use fetchProductCatalog() to fetch product details and display them.
 For each product, fetch the reviews using fetchProductReviews(productId).
 After fetching products and reviews, retrieve the sales report using fetchSalesReport().*/
+import { error } from "console";
 import {
   Product,
   Reviews,
@@ -27,10 +28,17 @@ function displayProductData() {
 
       //Fetch reviews for each product
       const reviewPromises = products.map((product) =>
-        fetchProductReviews(product.id).then((reviews: Reviews[]) => {
-          console.log(`\nReviews for ${product.name} (ID: ${product.id}):`);
-          console.table(reviews);
-        })
+        fetchProductReviews(product.id)
+          .then((reviews: Reviews[]) => {
+            console.log(`\nReviews for ${product.name} (ID: ${product.id}):`);
+            console.table(reviews);
+          })
+          .catch((error) => {
+            console.error(
+              `Error Fetching Reviews for ${product.name} (ID: ${product.id}):`,
+              error
+            );
+          })
       );
 
       //Wait for all reviews to be fetched
@@ -40,10 +48,20 @@ function displayProductData() {
       console.log("\nFetching sales report...");
 
       //Fetch and display sales report
-      return fetchSalesReport().then((salesReport: Sales) => {
-        console.log("\nSales Report:");
-        console.table([salesReport]);
-      });
+      return fetchSalesReport()
+        .then((salesReport: Sales) => {
+          console.log("\nSales Report:");
+          console.table([salesReport]);
+        })
+        .catch((error) => {
+          console.error("Error fetching sales report:", error);
+        });
+    })
+    .catch((error) => {
+      console.error("Error fetching product catalog:", error);
+    })
+    .finally(() => {
+      console.log("\nAll api calls have been checked");
     });
 }
 
